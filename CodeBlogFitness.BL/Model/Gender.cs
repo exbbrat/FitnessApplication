c1766a95f9a22c22cc485a4e743e1f4f540885fd
exbbrat <32882378+exbbrat@users.noreply.github.com>
2020-04-12 12:22:27 +0300

    Создана Модель Пользователя

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {       /// <summary>
+            ///  Название   
+            /// </summary>
+       
+        public string Name { get; }
+        /// <summary>
+        /// Cоздать новый пол.
+        /// </summary>
+        /// <param name="name"> Имя пола</param>
+        
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null ", nameof(name));
+            }
+
+            Name = name;
+
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return Name  ;
+        }
+    }
+}
---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
+   public class User
+    {/// <summary>
+     /// Имя.
+     /// </summary>
+     /// 
+        #region Свойства Пользователя
+        public string Name { get; }
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public  Gender Gender { get ; }
+        /// <summary>
+        /// Дата Рождения.
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// Вес.
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// Рост.
+        /// </summary>
+        /// 
+        public double Height { get; set; }
+        
+        #endregion
+        /// <summary>
+        /// Создать нового пользователя.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="gender"> Пол.</param>
+        /// <param name="birthDate">Дата Рождения.</param>
+        /// <param name="weight">Вес.</param>
+        /// <param name="height">Рост.</param>
+        /// 
+
+
+
+        public User( string name ,
+                     Gender gender,
+                     DateTime birthDate,
+                     double weight,
+                     double height)
+        {
+            #region Проверка Условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или Null.", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть null .",nameof(gender));
+            }
+            if(birthDate < DateTime.Parse("01.01.1900") || birthDate >= DateTime.Now)
+            {
+                throw new ArgumentException("Невозможная дата рождения.", nameof(birthDate));
+            }
+            if(weight <= 0)
+            {
+                throw new ArgumentException("Вес не может быть меньше или равен нулю.", nameof(weight));
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост не может быть меньше или равен нулю.", nameof(weight));
+            }
+            #endregion
+                
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
